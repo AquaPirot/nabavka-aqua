489819d23f7d9b151aa20534d244e3003c010451
AquaPirot <acaglobus@gmail.com>
2024-11-04 14:59:22 +0100

    Create index.js

---
+++ components/index.js
@@ -0,0 +1,7 @@
+// components/index.js
+export { default as Layout } from './Layout'
+export { default as AdminLayout } from './AdminLayout'
+export { default as RestaurantLayout } from './RestaurantLayout'
+export { default as LoadingSpinner } from './LoadingSpinner'
+export { default as ErrorMessage } from './ErrorMessage'
+export { default as SuccessMessage } from './SuccessMessage'
